b955b6d48583f1ffa85b50c753af767d37fbef1c
MetaMiku <MetaMiku@hotmail.com>
2025-11-16 14:15:53 +0800

    feat: press `RightShift` to log hello world

--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,69 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
+using BepInEx.Configuration;
 using UnityEngine;
 using UnityEngine.UI;
 using HarmonyLib;
-using System.Runtime.Serialization.Formatters;
-using System.Collections.Generic;
+using Il2CppInterop.Runtime.Injection;
+using UnityEngine.SceneManagement;
+using System;
 
 namespace MetaMystia;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BasePlugin
 {
-    internal static new ManualLogSource Log;
+    public static Plugin Instance;
+
+    public Action<Scene, LoadSceneMode> LoadAction;
+
+    public Plugin()
+    {
+        Instance = this;
+    }
 
     public override void Load()
     {
-        // Plugin startup logic
-        Log = base.Log;
         Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        try {
+            ClassInjector.RegisterTypeInIl2Cpp<PluginManager>();
+            Log.LogInfo("Registered C# Types in Il2Cpp");
+        }
+        catch {
+            Log.LogError("FAILED to Register Il2Cpp Type!");
+        }
+
+        try {
+            var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+            var originalHandle = AccessTools.Method(typeof(CanvasScaler), "Handle");
+            var postHandle = AccessTools.Method(typeof(BootstrapPatch), "Handle");
+            harmony.Patch(originalHandle, postfix: new HarmonyMethod(postHandle));
+        }
+        catch {
+            Log.LogError("FAILED to Apply Hooks!");
+        }
+    }
+
+    class BootstrapPatch
+    {
+        [HarmonyPostfix]
+        static void Handle()
+        {
+            if (PluginManager.Instance == null) {
+                Plugin.Instance.Log.LogMessage("Bootstrapping Trainer...");
+                try {
+                    PluginManager.Create("PluginManager");
+                    if (PluginManager.Instance != null) {
+                        Plugin.Instance.Log.LogMessage("Trainer Bootstrapped!");
+                    }
+                }
+                catch (Exception e) {
+                    Plugin.Instance.Log.LogMessage($"ERROR Bootstrapping Trainer: {e.Message}");
+                }
+            }
+        }
     }
 }
---
+++ PluginManager.cs
@@ -0,0 +1,33 @@
+using BepInEx.Logging;
+using UnityEngine;
+using System;
+using Il2CppInterop.Runtime;
+
+namespace MetaMystia;
+public class PluginManager : MonoBehaviour
+{
+    public static PluginManager Instance { get; private set; }
+    public static ManualLogSource Log => Plugin.Instance.Log;
+
+    public PluginManager(IntPtr ptr) : base(ptr)
+    {
+        Instance = this;
+    }
+
+    internal static GameObject Create(string name)
+    {
+        var gameObject = new GameObject(name);
+        DontDestroyOnLoad(gameObject);
+
+        var component = new PluginManager(gameObject.AddComponent(Il2CppType.Of<PluginManager>()).Pointer);
+
+        return gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightShift)) {
+            Log.LogMessage("hello world from MetaMystia");
+        }
+    }
+}
